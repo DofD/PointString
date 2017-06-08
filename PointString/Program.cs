9a7ae3581ea4d5e8d1e972150db42fb000f80186
Дмитрий <demonofdarkness@mail.ru>
2017-06-08 11:27:42 +0300

    + Пример

---
+++ PointString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PointString
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            unsafe
+            {
+                const string str = "Привет";
+                const string str2 = "Привет";
+
+                Console.WriteLine($"str = {str}");
+                Console.WriteLine($"str2 = {str2}");
+
+                fixed (char* p = str)
+                {
+                    var adr = (uint)p;
+                    Console.WriteLine($"Адресс str = {adr}");
+                    Console.WriteLine($"Адресс str2 = {adr}");
+
+                    p[0] = 'T';
+                }
+
+                Console.WriteLine($"str = {str}");
+                Console.WriteLine($"str2 = {str2}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
